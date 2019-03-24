6a9076a132a96d6238ca4d5c428bbd50582dbf7d
Ungarala Suresh <iamuvvsuresh@gmail.com>
2019-03-24 18:30:27 +0530

    added cors

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
 import * as dotenv from 'dotenv';
 import * as express from 'express';
+import cors = require('cors');
+import * as data from './test-data.json';
 
 console.log('----------------------------');
 
 const app = express();
+app.use(cors());    //using default config to avoid CORB (content origin read blocking)
 dotenv.config();
+
+//app.options('*', cors());   //mandate OPTIONS request before every API
 
 console.log(process.env.PORT);
 
 app.get('/', (req: express.Request, res: express.Response) => {
-    res.send('Hey! I am Server.');
+    res.send(data);
 });
 
 app.listen(process.env.PORT, () => {
